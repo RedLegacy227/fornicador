9adc82779c9cd7e66ccb86cf1a720c8000c1c0f2
RedLegacy227 <142516591+RedLegacy227@users.noreply.github.com>
2023-12-29 12:27:30 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -348,8 +348,8 @@ with tab1:
     filtro_Over_ht = (
         (df_over_ht05.Media_CG_H >= 3.8)
         & (df_over_ht05.Media_CG_A >= 3.8)
-        & (df_over_ht05.Media_Total_1HT_H >= 0.65)
-        & (df_over_ht05.Media_Total_1HT_A >= 0.65)
+        & (df_over_ht05.Media_Saldo_Golos_H_HT > 1)
+        & (df_over_ht05.Media_Saldo_Golos_A_HT > 1)
         & (df_over_ht05.CV_CG_H <= 0.9)
         & (df_over_ht05.CV_CG_A <= 0.9)
     )
